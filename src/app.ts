d63ec6b23ec666c7dd9818dab1a52c30aed527a8
Billrawknow <bildadronoh@gmail.com>
2024-06-11 22:50:03 +0300

    updated

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ async function main() {
   try {
     await server.listen(3000, "0.0.0.0");
 
-    console.log(`Server is running at http://localhost:3000`);
+    console.log(`Server started at http://localhost:3000`);
   } catch (e) {
     console.error(e);
     process.exit(1);
